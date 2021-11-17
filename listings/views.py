08f1dba4ca11839bf2b80464a02045c619c4deaa
oybek703 <hhoybek@gmail.com>
2021-11-17 18:36:05 +0500

    search form template part finished

--- listings/views.py
+++ listings/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Listing
 from django.core.paginator import Paginator
+from .choices import state_choices, bedroom_choices, price_choices
 
 
 def index(request):
@@ -19,4 +20,9 @@ def listing(request, listing_id):
 
 
 def search(request):
-    return render(request, 'listings/search.html')
+    context = {
+        'price_choices': price_choices,
+        'bedroom_choices': bedroom_choices,
+        'state_choices': state_choices
+    }
+    return render(request, 'listings/search.html', context)
--- pages/views.py
+++ pages/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 from listings.models import Listing
 from realtors.models import Realtor
+from listings.choices import price_choices, state_choices, bedroom_choices
 
 
 def index(request):
     listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
-    context = {'listings': listings}
+    context = {
+        'listings': listings,
+        'price_choices': price_choices,
+        'state_choices': state_choices,
+        'bedroom_choices': bedroom_choices
+    }
     return render(request, 'pages/index.html', context)
 
 
