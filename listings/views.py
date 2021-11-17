19641b455489da668c1b29e752c4ebcbc3534333
oybek703 <hhoybek@gmail.com>
2021-11-17 16:57:40 +0500

    home and about dynamic content almost finished

--- listings/views.py
+++ listings/views.py
@@ -1,15 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Listing
+from django.core.paginator import Paginator
 
 
 def index(request):
-    listings = Listing.objects.all()
-    context = {'listings': listings}
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)
+    paginator = Paginator(listings, 3)
+    page_number = request.GET.get('page')
+    paged_listings = paginator.get_page(page_number)
+    context = {'listings': paged_listings}
     return render(request, 'listings/listings.html', context)
 
 
 def listing(request, listing_id):
-    return render(request, 'listings/listing.html')
+    listing_obj = get_object_or_404(Listing, pk=listing_id)
+    context = {'listing': listing_obj}
+    return render(request, 'listings/listing.html', context)
 
 
 def search(request):
