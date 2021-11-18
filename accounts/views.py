85d305b32b4901fc04407c313da53d8beaf0b27f
oybek703 <hhoybek@gmail.com>
2021-11-18 18:53:42 +0500

    basic login and register view started

---
+++ accounts/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render, redirect
+
+
+def login(request):
+    return render(request, 'accounts/login.html')
+
+
+def register(request):
+    return render(request, 'accounts/register.html')
+
+
+def logout(request):
+    return redirect(request, 'index')
+
+
+def dashboard(request):
+    return render(request, 'accounts/dashboard')
